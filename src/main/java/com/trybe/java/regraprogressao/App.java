bd7fb83e6304fd632a2af582eb21fd566110a3d6
Jaci-Xavier <devxvr@gmail.com>
2023-10-24 18:56:56 -0300

    fix: print da nota req 02

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -22,20 +22,22 @@ public class App {
     double[] nota = new double[quantidadeAtividades];
 
     double somaPeso = 0;
-    double somaNota = 0;
+    // double somaNota = 0;
 
     for (int i = 0; i < quantidadeAtividades; i++) {
       System.out.println("Digite o nome da atividade " + (i + 1) + ": ");
       nome[i] = scanner.nextLine();
+      
       System.out.println("Digite o peso da atividade " + (i + 1) + ": ");
       peso[i] = scanner.nextDouble();
       scanner.nextLine();
-      System.out.println("Digite a nota da atividade " + (i + 1) + ": ");
+
+      System.out.println("Digite a nota obtida para " + nome[i] + ": ");
       nota[i] = scanner.nextDouble();
       scanner.nextLine();
 
-      somaPeso += peso[i];
-      somaNota += (peso[i] * nota[i]) / 100;
+      // somaPeso += peso[i];
+      // somaNota += (peso[i] * nota[i]) / 100;
     }
 
     if (somaPeso != 100) {
