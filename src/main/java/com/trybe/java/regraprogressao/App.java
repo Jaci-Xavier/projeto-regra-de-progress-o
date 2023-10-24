e2ac81b10a80c6d0eeef5a6cdf6ad04937667f53
Jaci-Xavier <devxvr@gmail.com>
2023-10-24 18:13:46 -0300

    iniciando projeto e fazendo req 01

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,7 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * App.
  */
@@ -9,5 +11,22 @@ public class App {
    * Metodo main.
    */
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Digite a quantidade de atividades para cadastrar: ");
+
+    int quantidadeAtividades = scanner.nextInt();
+    scanner.nextLine();
+
+    String[] nome = new String[quantidadeAtividades];
+    double[] peso = new double[quantidadeAtividades];
+
+    for (int i = 0; i < quantidadeAtividades; i++) {
+      System.out.println("Digite o nome da atividade " + (i + 1) + ": ");
+      nome[i] = scanner.nextLine();
+      System.out.println("Digite o peso da atividade " + (i + 1) + ": ");
+      peso[i] = scanner.nextDouble();
+      scanner.nextLine();
+    }
+    scanner.close();
   }
 }
