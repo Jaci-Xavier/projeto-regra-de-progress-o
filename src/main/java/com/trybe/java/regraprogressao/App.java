720ca553317a1bdc2e0d27dfab0f6426d5eb52c0
Jaci-Xavier <devxvr@gmail.com>
2023-10-25 17:05:57 -0300

    concluindo projeto

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -22,31 +22,43 @@ public class App {
     double[] nota = new double[quantidadeAtividades];
 
     int somaPeso = 0;
-    // double somaNota = 0;
+    double somaNota = 0;
 
     for (int i = 0; i < quantidadeAtividades; i++) {
       System.out.println("Digite o nome da atividade " + (i + 1) + ": ");
-      scanner.nextLine();
       nome[i] = scanner.nextLine();
       
       System.out.println("Digite o peso da atividade " + (i + 1) + ": ");
       peso[i] = scanner.nextDouble();
+      scanner.nextLine();
       
       System.out.println("Digite a nota obtida para " + nome[i] + ": ");
       nota[i] = scanner.nextDouble();
-      // scanner.nextLine();
+      scanner.nextLine();
 
       somaPeso += peso[i];
 
-      // somaNota += (peso[i] * nota[i]) / 100;
+      somaNota += (peso[i] * nota[i]) / 100;
     }
 
     if (somaPeso != 100) {
       System.out.println("A soma dos pesos é diferente de 100!");
     }
-    // if (somaPeso == 100) {
-    //   System.out.println("Atividades cadastradas com sucesso!");
-    // }
+
+    double mediaDeNotas = somaNota * 100 / somaPeso;
+
+    if (mediaDeNotas >= 85.0) {
+      System.out.println(
+          "Parabéns! Você alcançou " + mediaDeNotas 
+           + "%! E temos o prazer de informar que você obteve aprovação!");
+    } else {
+      System.out.println(
+          "Lamentamos informar que, com base na sua pontuação alcançada neste período, "
+            + 
+            mediaDeNotas
+            + 
+            "%, você não atingiu a pontuação mínima necessária para sua aprovação.");
+    }
     scanner.close();
   }
 }
